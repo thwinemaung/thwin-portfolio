ee1a46a8d65d32d3527288c09cad7af4518051dd
thwinemaung <thwinemaung@gmail.com>
2023-04-26 16:16:02 +0630

    added projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About"
+import ContactMe from "@/components/ContactMe"
 import Header from "@/components/Header"
 import Hero from "@/components/Hero"
 import Projects from "@/components/Projects"
@@ -9,7 +10,7 @@ import Head from "next/head"
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll z-0 overflow-x-hidden">
       <Head>
         <title>Portfolio</title>
       </Head>
@@ -36,7 +37,9 @@ export default function Home() {
         <Projects />
       </section>
 
-      {/* Contact Me */}
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
     </div>
   )
 }
